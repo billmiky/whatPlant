ed156d49c0b2166494f3b9f5263c8a5f3884c360
Will Pollard <billmiky@gmail.com>
2012-02-22 17:50:48 -0500

    Iniital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatPlant::Application.config.secret_token = '36112b178279d37d2f266a6c600b5078073990646f6657daf4e6ea28e8ccf636acdfc9dd39940665b62a1593202bb44424a3df2ce24c5d7b639272dff48ca11d'
